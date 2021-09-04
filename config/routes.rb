b6b46445d5348193ab59eebeb6508606eaceb60e
EC2 Default User <ec2-user@ip-172-31-42-57.ec2.internal>
2021-09-04 02:09:43 +0000

    [Add] indexのレイアウト

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   get 'home' => 'homes#top'
+   get 'books' => 'books#index'
+   get 'books/new'
+   post 'books' => 'books#create'
+  get 'books/:id' => 'books#show',  as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+end
